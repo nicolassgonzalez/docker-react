f2f98d7f7936d1997b103437e85e2871199d4bb7
nicolasgonzalez <nicolasgonzalez@santandertecnologia.com.ar>
2021-12-29 02:04:49 -0300

    new text

--- src/App.js
+++ src/App.js
@@ -7,15 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>New feature!</p>
+
       </header>
     </div>
   );
